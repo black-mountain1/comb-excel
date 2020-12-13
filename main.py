9a06962bd0faaf6ce2c72b351232c027ac75cb30
Paul Trouton <pault_25@hotmail.com>
2020-12-13 21:45:28 +0000

    modified into functions

--- main.py
+++ main.py
@@ -2,46 +2,86 @@ import pandas as pd
 import glob
 import argparse
 
-parser = argparse.ArgumentParser(description='Tool to combine monthly sales reports.')
-parser.add_argument('data_dir', help='Directory where the sales data is stored')
-parser.add_argument('output_dir', help='Directory where the summary report will be stored')
-parser.add_argument('cust_file', help='Customer account status file')
-parser.add_argument('-d', help='Start date to include')
 
-args = parser.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tool to combine monthly sales reports.')
+    parser.add_argument('data_dir', help='Directory where the sales data is stored')
+    parser.add_argument('output_dir', help='Directory where the summary report will be stored')
+    parser.add_argument('cust_file', help='Customer account status file')
+    parser.add_argument('-d', help='Start date to include')
 
-
-input_dir = args.data_dir
-dest = args.output_dir
-status = args.cust_file
-date = args.d
-
-status = pd.read_excel(status + '/customer-status.xlsx')
-print(f'Shape of status: {status.shape}')
-
-all_data = pd.DataFrame()
-for file in glob.glob(input_dir + '/sales-*-*.xlsx'):
-    df = pd.read_excel(file)
-    all_data = all_data.append(df, ignore_index=True)
+    args = parser.parse_args()
+    return args
 
 
-print("Shape of all data: ", all_data.shape)
-print("Data types: ", all_data.dtypes)
+def read_sales(dir):
+    """
+    Reads sales data files in the format sales-*-*.xlsx from the directory.
+    :param dir: Location of sales data files
+    :return: DataFrame of combined sales data
+    """
+    all_data = pd.DataFrame()
+    for file in glob.glob(dir + '/sales-*-*.xlsx'):
+        df = pd.read_excel(file)
+        all_data = all_data.append(df, ignore_index=True)
 
-for col in all_data.columns:
-    pct_missing = all_data[col].isna().sum()
-    print(f"{col} column has {pct_missing} missing values")
+    return all_data
 
-all_data['date'] = pd.to_datetime(all_data['date'])
 
-all_data_st = all_data.merge(status, how='left')
+def read_cust_file(dir):
+    """
+    Reads the customer status file 'customer-status.xlsx in the directory.
+    :param dir: Location of the customer file
+    :return: DataFrame of the customer data
+    """
+    df = pd.read_excel(dir + '/customer-status.xlsx')
+    return df
 
-all_data_st.status.fillna('bronze', inplace=True)
-all_data_st['status'] = all_data_st['status'].astype('category')
-all_data_st['status'].cat.set_categories(['gold', 'silver', 'bronze'], inplace=True)
-all_data_st = all_data_st.sort_values(by='status')
 
-output_file = '/summary_report.xlsx'
+def process_data(all_data, cust_data):
+    """
+    Processes the sales data by joining it with the customer data and sorting on status category.
+    :param all_data: DataFrame of the sales data
+    :param cust_data: DataFrame of the customer data
+    :return: DataFrame of the combined sales and customer data
+    """
+    for col in all_data.columns:
+        pct_missing = all_data[col].isna().sum()
+        print(f"{col} column has {pct_missing} missing values")
 
-with pd.ExcelWriter(dest + output_file) as writer:
-    all_data_st.to_excel(writer)
+    all_data['date'] = pd.to_datetime(all_data['date'])
+
+    all_data_st = all_data.merge(cust_data, how='left')
+
+    all_data_st.status.fillna('bronze', inplace=True)
+    all_data_st['status'] = all_data_st['status'].astype('category')
+    all_data_st['status'].cat.set_categories(['gold', 'silver', 'bronze'], inplace=True)
+    all_data_st = all_data_st.sort_values(by='status')
+
+    return all_data_st
+
+
+def write_summary(dir, df):
+    """
+    Writes the DataFrame to the directory as summary_report.xlsx
+    :param dir: Directory to save the report
+    :param df: DataFrame of the summary report to be stored
+    :return: No return
+    """
+    output_file = '/summary_report.xlsx'
+
+    with pd.ExcelWriter(dir + output_file) as writer:
+        df.to_excel(writer)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    print('Reading sales data')
+    sales_data = read_sales(args.data_dir)
+    print('Reading customer data')
+    cust = read_cust_file(args.cust_file)
+    print('Processing data')
+    processed_data = process_data(sales_data, cust)
+    print('Saving summary report')
+    write_summary(args.output_dir, processed_data)
+    print('Script complete')
